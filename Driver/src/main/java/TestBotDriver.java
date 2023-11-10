5613488a304178d4352fb657bf1697604142c21e
ARGamer36 <ajrodz2004@gmail.com>
2023-11-09 21:54:26 -0500

    Attempting to add new Driver Module

--- Driver/src/main/java/TestBotDriver.java
+++ Driver/src/main/java/TestBotDriver.java
@@ -1,17 +1,8 @@
-import net.dv8tion.jda.api.entities.Activity;
-
 import javax.security.auth.login.LoginException;
 
 public class TestBotDriver extends TestBot {
-    @Override
-    protected void setup() {
-        VERSION = "1.0";
-        activity = Activity.watching("Test runs!!!");
-        TOKEN = "TEST_BOT";
-    }
     public TestBotDriver() throws LoginException {
         super();
-        shardManager.addEventListener(new TestMain(VERSION));
     }
     public static void main(String[] args) {
         try {
